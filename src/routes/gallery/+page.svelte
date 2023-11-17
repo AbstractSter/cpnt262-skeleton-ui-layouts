e33f6fe86dc08461a8e59ac6c5c9e14e3422cd03
Sterling Smith <sterling12311@gmail.com>
2023-11-17 14:40:10 -0700

    changed size of first card in gallery

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,8 +1,3 @@
-<!-- <script>
-	import { card } from 'card.svelte';
-</script> -->
-
-
 <section class="flex">
 	<!-- Card 1 -->
 	<div class="container flex-start justify-center items-center p-10">
@@ -10,7 +5,7 @@
 			<div class="card">
 				<header class="card-header text-2xl">This is the first card</header>
 				<section class="p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, maxime.</section>
-				<img src="images/pexels-dids-2911521.jpg" alt="abstract pink, black and yellow painting">
+				<img src="images/pexels-dids-2911521.jpg" alt="abstract pink, black and yellow painting" class="w-auto mx-auto h-[22rem]">
 				<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click for more info!</button>
 				</div>
 			</div>
@@ -40,37 +35,3 @@
 			</div>
 		</div>
 </section>
-
-<!-- <style lang="postcss">
-	figure {
-		@apply flex relative flex-col;
-	}
-	figure svg,
-	.img-bg {
-		@apply w-64 h-64 md:w-80 md:h-80;
-	}
-	.img-bg {
-		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
-			glow 5s linear infinite;
-	}
-	@keyframes glow {
-		0% {
-			@apply bg-primary-400/50;
-		}
-		33% {
-			@apply bg-secondary-400/50;
-		}
-		66% {
-			@apply bg-tertiary-400/50;
-		}
-		100% {
-			@apply bg-primary-400/50;
-		}
-	}
-	@keyframes pulse {
-		50% {
-			transform: scale(1.5);
-		}
-	}
-</style> -->
